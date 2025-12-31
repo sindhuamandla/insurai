e36567434f16cff18008283018b8a1904e605ad7
sindhu <148188793+sindhuamandla@users.noreply.github.com>
2025-12-31 15:10:20 +0530

    Add files via upload

---
+++ src/main/java/com/example/InsurAI/InsurAiApplication.java
@@ -0,0 +1,11 @@
+package com.example.InsurAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.example.InsurAI")
+public class InsurAiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(InsurAiApplication.class, args);
+	}
+}
